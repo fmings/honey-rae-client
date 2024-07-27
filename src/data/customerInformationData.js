b79333c511021b963b86efc6b27e599614cdde76
fmings <mingsfelicia@gmail.com>
2024-07-27 11:33:04 -0500

    added list and detail view for customers

---
+++ src/data/customerInformationData.js
@@ -0,0 +1,17 @@
+const _apiUrl = "/api/customers";
+
+export const getCustomers = () => {
+  return fetch(_apiUrl).then((r) => r.json());
+};
+
+export const getCustomer = (id) => new Promise((resolve, reject) => {
+  fetch(`/api/customers/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+})
